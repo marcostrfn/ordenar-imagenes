dd923e0d5989b028b65d4881eae426d2a6ccfc33
marcostrfn <marcos.trfn@gmail.com>
2017-03-09 13:51:07 +0100

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,39 @@
+
+import os
+import exiftool
+
+from funciones import module
+
+
+def main():
+	
+	dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'img')	
+	matches = module.get_files_recursive(dir, [".jpg"])
+		
+	if len(matches)==0:
+		sys.exit()
+		
+	with exiftool.ExifTool() as et:
+		metadata = et.get_metadata_batch(matches)
+	
+	print 
+	for d in metadata:
+	
+		
+		dname = os.path.dirname(d["SourceFile"])
+		bname = os.path.basename(d["SourceFile"])
+		# las imagenes de wasap vienen con pocos metadatos
+		try:
+			dateOriginal = d["EXIF:DateTimeOriginal"]		
+		except:
+			dateOriginal = None
+			
+		print dname, bname, dateOriginal
+		
+		# para ver todas las claves de la imagen
+		# for key in sorted(d):
+		# 	print ( "{0} {1}".format(key,d[key]) )
+		
+
+
+if __name__ == '__main__': main()
