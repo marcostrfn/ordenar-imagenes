4d79870715463b23cbeb127f86ac0a25c5163570
marcostrfn <marcos.trfn@gmail.com>
2017-03-14 21:18:07 +0100

    Eliminando exiftool

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 ﻿
 import os
 import sys
-import exiftool
 import exifread
 import re
 
@@ -32,38 +31,6 @@ def mainWin():
 					print ("{0:20}\t\t{1}".format(tag, tags[tag]))
 
 		print
-
-
-
-def main():
-
-	dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'img')
-	matches = module.get_files(dir, [".jpg"])
-
-	if len(matches)==0:
-		sys.exit()
-
-	with exiftool.ExifTool() as et:
-		metadata = et.get_metadata_batch(matches)
-
-	print
-	for d in metadata:
-
-
-		# dname = os.path.dirname(d["SourceFile"])
-		# bname = os.path.basename(d["SourceFile"])
-		# para imagenes con pocos metadatos
-		# try:
-		#	dateOriginal = d["EXIF:DateTimeOriginal"]
-		# except:
-		# 	dateOriginal = None
-
-		# print dname, bname, dateOriginal
-
-		# para ver todas las claves de la imagen
-		for key in sorted(d):
-			print ( "{0} {1}".format(key,d[key]) )
-
 
 
 if __name__ == '__main__': mainWin()
