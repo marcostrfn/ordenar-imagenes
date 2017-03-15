4ff25e3458c0917cc28c1c4f1697e538436cd4b4
marcostrfn <marcos.trfn@gmail.com>
2017-03-15 13:32:42 +0100

    Add files via upload
    ConfigParser, sin mongoDb

--- __main__.py
+++ __main__.py
@@ -1,31 +1,37 @@
 ﻿
 import os
 import sys
-import exifread
 import re
-import pymongo
+
+import exifread
+import pickle
+import json
 
 
 from funciones import module
 
-
-def mainWin():
-	# conexion con mongo
-	client = pymongo.MongoClient('mongodb://192.168.1.36:27017/')
-	db = client.imagenes
-	posts = db.data
-	post_id = posts.insert_one({"x" : "1"}).inserted_id
-	print post_id
-	return
+def verFichero():
 	
-    # https://pypi.python.org/pypi/ExifRead
+	file_data = module.get_data_data()
+	info = pickle.load(open( file_data, "rb" ))
+	for line in info:
+		print json.dumps(line, indent=4, sort_keys=True)		
+		if "Image XResolution" in line:
+			print line[r'Image XResolution']
+	
 
-	# dir = r"C:\tmp"
-	dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),'img')
-	files = module.get_files(dir, [".png", ".jpg"])
+
+
+def main():
+	
+	info = []	
+	
+	dir_source, dir_dest, dir_data, file_data, types = module.get_data_config()			
+	files = module.get_files(dir_source, types)
+	
 	for file in files:
 		print file
-		print "=" * len(file)
+		# print "=" * len(file)
 
 		# Open image file for reading (binary mode)
 		# and return Exif tags
@@ -33,14 +39,27 @@ def mainWin():
 		tags = exifread.process_file(f)
 		f.close()
 
+		new_tag = {}
+		new_tag['filename'] = os.path.basename(file)
+		new_tag['dirname'] = os.path.dirname(file)
+		
 		for tag in sorted(tags.keys()):
 			match = re.search("thumb", tag, re.IGNORECASE)
 			if not match:
-				match = re.search("^(EXIF|image){1}\s(model|datetimeoriginal){1}", tag, re.IGNORECASE)
-				if match:
-					print ("{0:20}\t\t{1}".format(tag, tags[tag]))
+				new_tag[tag] = tags.get(tag).printable
+				# match = re.search("^(EXIF|image){1}\s(model|datetimeoriginal){1}", tag, re.IGNORECASE)
+				# if match:					
+					# new_tag.append((tag,tags.get(tag).printable))
+					# print ("{0:20}\t\t{1}".format(tag, tags[tag]))
+		
+		# print
+		info.append(new_tag)
 
-		print
-
-
-if __name__ == '__main__': mainWin()
+	
+	pickle.dump( info, open( file_data, "wb" ) )
+	return
+	
+		
+if __name__ == '__main__':
+	main()
+	verFichero()
